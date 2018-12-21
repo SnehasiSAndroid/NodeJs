8487b797a1cc64002c44c8e442071f624cd0a29a
Mallikarjun <mallikarjun@mobi2fun.com>
2018-12-21 17:41:52 +0530

    modified

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First page</title></head>');
+    res.write('<body>Hello from Node Js server</body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
