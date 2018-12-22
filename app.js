875efa11cb99f18afb5b62bf7350571e8ed32709
Snehasis Sasmal <connect2snehasis@gmail.com>
2018-12-23 01:39:35 +0530

    added routes

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const http = require('http');
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
+const routes = require('./routes');
 
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First page</title></head>');
-    res.write('<body>Hello from Node Js server</body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
