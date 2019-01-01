86676b5fc38d6913995cd758d7863453f65f949f
Snehasis Sasmal <connect2snehasis@gmail.com>
2019-01-01 22:56:41 +0530

    added templating engine

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
@@ -13,11 +14,15 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/admin',adminRoutes);
+//app.engine('hbs',expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404',{pageTitle: 'Page Not Found'});
 })
 
 
